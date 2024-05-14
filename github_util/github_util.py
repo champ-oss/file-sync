ce753b6a2092ae828d6780d4334b2a92199cf6d5
Justin Reeves <reeves122@gmail.com>
2024-05-14 15:34:24 -0500

    refactor to load one repository per github session

--- github_util/github_util.py
+++ github_util/github_util.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 class GitHubUtil:
     """Provides functionality for interfacing with GitHub repositories."""
 
-    def __init__(self: Self, access_token: str, github_session: Github = None) -> None:
+    def __init__(self: Self, access_token: str, repository_name: str, github_session: Github = None) -> None:
         """
         Initialize the GitHub utility.
 
@@ -24,132 +24,150 @@ class GitHubUtil:
         :param github_session: authenticated session to GitHub
         """
         if not github_session:
-            logger.info('logging in to GitHub using access token')
+            logger.debug('logging in to GitHub using access token')
             self.github_session = Github(auth=Auth.Token(access_token))
+            self.repository = self._get_repo(self.github_session, repository_name)
+
         else:
             self.github_session = github_session
 
-    def get_repo(self: Self, repo_name: str) -> Optional[Repository]:
+    @staticmethod
+    def _get_repo(github_session: Github, repository_name: str) -> Optional[Repository]:
         """
         Get a repository by name in the format of 'owner/repo'.
 
-        :param repo_name: name of the repository
+        :param github_session: authenticated session to GitHub
+        :param repository_name: name of the repository
         :return: GitHub repository
         """
         try:
-            return self.github_session.get_repo(repo_name)
+            return github_session.get_repo(repository_name)
         except (UnknownObjectException, GithubException) as e:
-            logger.warning(f'unable to find repository: {repo_name} error:{e}')
+            logger.error(f'unable to find repository: {repository_name} error:{e}')
             return None
 
-    @staticmethod
-    def create_branch_if_not_exists(repo: Repository, branch_name: str, source_branch: str = 'main') -> None:
-        """
-        Create a branch on the repository if it does not already exist.
-
-        :param repo: repository to create branch
-        :param branch_name: name of the branch to create
-        :param source_branch: source of the new branch (default: main)
-        :return: None
-        """
-        try:
-            branch = repo.get_branch(branch_name)
-            if branch:
-                return
-        except (UnknownObjectException, GithubException) as e:
-            logger.debug(e)
-
-        logger.info(f'{repo.name}: creating {branch_name} branch')
-        source_sha = repo.get_branch(source_branch).commit.sha
-        repo.create_git_ref(ref=f'refs/heads/{branch_name}', sha=source_sha)
-
-    @staticmethod
-    def is_file_up_to_date(source_file: FileConfig, destination_repo: Repository, branch: str) -> bool:
-        """
-        Check if the source file is up-to-date in the destination repository by comparing the SHA.
-
-        :param source_file: source file to check
-        :param destination_repo: repo where the file should be updated
-        :param branch: which branch the file should be checked
-        :return: true if the file is up-to-date, false if it needs to be updated
-        """
-        try:
-            logger.info(f'{destination_repo.name}: checking file: {source_file.destination_path}')
-            destination_file = destination_repo.get_contents(source_file.destination_path, ref=branch)
-        except (UnknownObjectException, GithubException) as e:
-            logger.debug(e)
-            return False
-
-        if destination_file.sha == source_file.sha:
-            logger.info(f'{destination_repo.name}: file is already up to date '
-                        f'on {branch} branch: {source_file.destination_path}')
-            return True
-
-        logger.warning(f'{destination_repo.name}: file needs to be updated '
-                       f'on {branch} branch: {source_file.destination_path}')
-        return False
-
-    @staticmethod
-    def update_file(destination_repo: Repository, sync_file: FileConfig, branch: str) -> None:
-        """
-        Update the file in the destination repository, or create a new file if it does not exist.
-
-        :param destination_repo: repository containing the file to update
-        :param sync_file: file to update
-        :param branch: branch to push changes
-        :return: None
-        """
-        try:
-            destination_file = destination_repo.get_contents(sync_file.destination_path, ref=branch)
-            logger.info(f'{destination_repo.name}: updating file: {sync_file.destination_path}')
-            destination_repo.update_file(path=sync_file.destination_path,
-                                         message='Updated by file-sync',
-                                         content=sync_file.content,
-                                         sha=destination_file.sha,
-                                         branch=branch)
-
-        except (UnknownObjectException, GithubException) as e:
-            logger.debug(e)
-            logger.info(f'{destination_repo.name}: creating file: {sync_file.destination_path}')
-            destination_repo.create_file(path=sync_file.destination_path,
-                                         message='Updated by file-sync',
-                                         content=sync_file.content,
-                                         branch=branch)
-
-    @staticmethod
-    def create_pull_request(destination_repo: Repository, head_branch: str, base_branch: str = 'main') -> None:
-        """
-        Create a pull request on the destination repository.
-
-        :param destination_repo: repository to create the pull request
-        :param head_branch: source branch for the pull request
-        :param base_branch: target branch for the pull request (default: main)
-        :return:
-        """
-        try:
-            pull_request = destination_repo.create_pull(title='file-sync', head=head_branch, base=base_branch)
-            logger.info(f'{destination_repo.name}: created pull request: {pull_request.html_url}')
-        except GithubException as e:
-            logger.debug(e)
-
-    def get_sync_files_from_source_repo(self: Self, source_repo_name: str, action_input_files: str,
-                                        source_branch: str = 'main') -> list[FileConfig]:
+    def get_sync_files_from_source_repo(self: Self, action_input_files: str,
+                                        branch: str = 'main') -> Optional[list[FileConfig]]:
         """
         Get the list of files to sync from the source repository.
 
-        :param source_repo_name: name of the source repository
         :param action_input_files: string of input files from the GitHub action
-        :param source_branch: name of the branch to sync files from (default: main)
+        :param branch: name of the branch to sync files from (default: main)
         :return: list of files to sync
         """
-        source_repo = self.github_session.get_repo(source_repo_name)
+        if not self.repository:
+            return None
+
         sync_files = ConfigUtil.parse_files_config_from_input(action_input_files)
 
         for sync_file in sync_files:
-            template_file = source_repo.get_contents(sync_file.source_path, ref=source_branch)
+            template_file = self.repository.get_contents(sync_file.source_path, ref=branch)
             sync_file.sha = template_file.sha
             sync_file.content = base64.b64decode(template_file.content)
             logger.info(f'loaded source file: {sync_file.source_path} sha:{sync_file.sha} '
                         f'bytes:{len(sync_file.content)}')
 
         return sync_files
+
+    def sync_files_for_repo(self: Self, sync_files: list[FileConfig]) -> None:
+        """
+        Sync the list of files to the repository.
+
+        :param sync_files: list of files to sync
+        :return: None
+        """
+        if not self.repository:
+            return None
+
+        for sync_file in sync_files:
+            if self._is_file_up_to_date(sync_file, branch='main'):
+                continue
+
+            self._create_branch_if_not_exists('file-sync')
+
+            if self._is_file_up_to_date(sync_file, branch='file-sync'):
+                continue
+
+            self._update_file(sync_file, branch='file-sync')
+
+    def _create_branch_if_not_exists(self: Self, branch_name: str, source_branch: str = 'main') -> None:
+        """
+        Create a branch on the repository if it does not already exist.
+
+        :param branch_name: name of the branch to create
+        :param source_branch: source of the new branch (default: main)
+        :return: None
+        """
+        try:
+            branch = self.repository.get_branch(branch_name)
+            if branch:
+                return
+        except (UnknownObjectException, GithubException) as e:
+            logger.debug(e)
+
+        logger.info(f'{self.repository.name}: creating {branch_name} branch')
+        source_sha = self.repository.get_branch(source_branch).commit.sha
+        self.repository.create_git_ref(ref=f'refs/heads/{branch_name}', sha=source_sha)
+
+    def _is_file_up_to_date(self: Self, source_file: FileConfig, branch: str) -> bool:
+        """
+        Check if the source file is up-to-date in the destination repository by comparing the SHA.
+
+        :param source_file: source file to check
+        :param branch: which branch the file should be checked
+        :return: true if the file is up-to-date, false if it needs to be updated
+        """
+        try:
+            logger.info(f'{self.repository.name}: checking file: {source_file.destination_path}')
+            destination_file = self.repository.get_contents(source_file.destination_path, ref=branch)
+        except (UnknownObjectException, GithubException) as e:
+            logger.debug(e)
+            return False
+
+        if destination_file.sha == source_file.sha:
+            logger.info(f'{self.repository.name}: file is already up to date '
+                        f'on {branch} branch: {source_file.destination_path}')
+            return True
+
+        logger.warning(f'{self.repository.name}: file needs to be updated '
+                       f'on {branch} branch: {source_file.destination_path}')
+        return False
+
+    def _update_file(self: Self, sync_file: FileConfig, branch: str) -> None:
+        """
+        Update the file in the destination repository, or create a new file if it does not exist.
+
+        :param sync_file: file to update
+        :param branch: branch to push changes
+        :return: None
+        """
+        try:
+            destination_file = self.repository.get_contents(sync_file.destination_path, ref=branch)
+            logger.info(f'{self.repository.name}: updating file: {sync_file.destination_path}')
+            self.repository.update_file(path=sync_file.destination_path,
+                                        message='Updated by file-sync',
+                                        content=sync_file.content,
+                                        sha=destination_file.sha,
+                                        branch=branch)
+
+        except (UnknownObjectException, GithubException) as e:
+            logger.debug(e)
+            logger.info(f'{self.repository.name}: creating file: {sync_file.destination_path}')
+            self.repository.create_file(path=sync_file.destination_path,
+                                        message='Updated by file-sync',
+                                        content=sync_file.content,
+                                        branch=branch)
+
+    def _create_pull_request(self: Self, head_branch: str, base_branch: str = 'main') -> None:
+        """
+        Create a pull request on the destination repository.
+
+        :param head_branch: source branch for the pull request
+        :param base_branch: target branch for the pull request (default: main)
+        :return:
+        """
+        try:
+            pull_request = self.repository.create_pull(title='file-sync', head=head_branch, base=base_branch)
+            logger.info(f'{self.repository.name}: created pull request: {pull_request.html_url}')
+        except GithubException as e:
+            logger.debug(e)
--- github_util/test_github_util.py
+++ github_util/test_github_util.py
@@ -15,101 +15,106 @@ class TestGitHubUtil(unittest.TestCase):
     def setUp(self: Self) -> None:
         """Set up the requirements for testing."""
         self.github_session = MagicMock()
-        self.github_util = GitHubUtil(access_token='test123', github_session=self.github_session)
+        self.github_util = GitHubUtil(access_token='test123', repository_name='test-repository',
+                                      github_session=self.github_session)
 
     def test_get_repo(self: Self) -> None:
         """Validate the get_repo function is successful."""
-        self.assertIsNotNone(self.github_util.get_repo(repo_name='my-org/test-repo-1'))
+        self.assertIsNotNone(self.github_util._get_repo(MagicMock(), 'my-org/test-repo-1'))
 
-        self.github_session.get_repo.side_effect = UnknownObjectException(400)
-        self.assertIsNone(self.github_util.get_repo(repo_name='my-org/test-repo-1'))
+        github_session = MagicMock()
+        github_session.get_repo.side_effect = UnknownObjectException(400)
+        self.assertIsNone(self.github_util._get_repo(github_session, 'my-org/test-repo-1'))
 
-        self.github_session.get_repo.side_effect = GithubException(status=422, message='Not found')
-        self.assertIsNone(self.github_util.get_repo(repo_name='my-org/test-repo-1'))
+        github_session = MagicMock()
+        github_session.get_repo.side_effect = GithubException(status=422, message='Not found')
+        self.assertIsNone(self.github_util._get_repo(github_session, 'my-org/test-repo-1'))
 
     def test_create_branch_if_not_exists_with_not_found_exception(self: Self) -> None:
         """Validate the create_branch_if_not_exists function is successful."""
-        repo = MagicMock()
+        self.github_util.repository = MagicMock()
         branch = MagicMock()
         branch.commit.sha = '123'
 
-        repo.get_branch.side_effect = [
+        self.github_util.repository.get_branch.side_effect = [
             GithubException(status=404, data={'message': 'Branch not found'}),
             branch
         ]
-        self.github_util.create_branch_if_not_exists(repo=repo, branch_name='test-branch')
-        repo.create_git_ref.assert_called_once()
-        repo.create_git_ref.assert_called_with(ref='refs/heads/test-branch', sha='123')
+        self.github_util._create_branch_if_not_exists(branch_name='test-branch')
+        self.github_util.repository.create_git_ref.assert_called_once()
+        self.github_util.repository.create_git_ref.assert_called_with(ref='refs/heads/test-branch', sha='123')
 
     def test_create_branch_if_not_exists_with_branch_already_exists(self: Self) -> None:
         """Validate the create_branch_if_not_exists function is successful."""
-        repo = MagicMock()
-        repo.get_branch.return_value = 'test-branch'
-        self.github_util.create_branch_if_not_exists(repo=repo, branch_name='test-branch')
-        repo.create_git_ref.assert_not_called()
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_branch.return_value = 'test-branch'
+        self.github_util._create_branch_if_not_exists(branch_name='test-branch')
+        self.github_util.repository.create_git_ref.assert_not_called()
 
     def test_is_file_up_to_date_should_return_true(self: Self) -> None:
         """Validate the is_file_up_to_date function is successful."""
         source_file = FileConfig(source_path='', destination_path='foo', sha='123')
-        destination_repo = MagicMock()
-        destination_repo.get_contents.return_value.sha = '123'
-        self.assertTrue(self.github_util.is_file_up_to_date(source_file, destination_repo, branch='main'))
-        destination_repo.get_contents.assert_called_with('foo', ref='main')
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.return_value.sha = '123'
+        self.assertTrue(self.github_util._is_file_up_to_date(source_file, branch='main'))
+        self.github_util.repository.get_contents.assert_called_with('foo', ref='main')
 
     def test_is_file_up_to_date_should_return_false(self: Self) -> None:
         """Validate the is_file_up_to_date function is successful."""
         source_file = FileConfig(source_path='', destination_path='foo', sha='123')
-        destination_repo = MagicMock()
-        destination_repo.get_contents.return_value.sha = '456'
-        self.assertFalse(self.github_util.is_file_up_to_date(source_file, destination_repo, branch='main'))
-        destination_repo.get_contents.assert_called_with('foo', ref='main')
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.return_value.sha = '456'
+        self.assertFalse(self.github_util._is_file_up_to_date(source_file, branch='main'))
+        self.github_util.repository.get_contents.assert_called_with('foo', ref='main')
 
     def test_is_file_up_to_date_should_return_false_when_file_not_found(self: Self) -> None:
         """Validate the is_file_up_to_date function is successful."""
-        destination_repo = MagicMock()
-        destination_repo.get_contents.side_effect = UnknownObjectException(404)
-        self.assertFalse(self.github_util.is_file_up_to_date(MagicMock(), destination_repo, branch='main'))
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.side_effect = UnknownObjectException(404)
+        self.assertFalse(self.github_util._is_file_up_to_date(MagicMock(), branch='main'))
 
-        destination_repo = MagicMock()
-        destination_repo.get_contents.side_effect = GithubException(status=404, data={'message': 'File not found'}),
-        self.assertFalse(self.github_util.is_file_up_to_date(MagicMock(), destination_repo, branch='main'))
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.side_effect = GithubException(status=404,
+                                                                               data={'message': 'File not found'}),
+        self.assertFalse(self.github_util._is_file_up_to_date(MagicMock(), branch='main'))
 
     def test_update_file_should_update_existing_file(self: Self) -> None:
         """Validate the update_file function is successful."""
         source_file = FileConfig(source_path='', destination_path='foo', content=b'foo')
-        destination_repo = MagicMock()
-        destination_repo.get_contents.return_value.sha = '456'
-        self.github_util.update_file(destination_repo, source_file, branch='main')
-        destination_repo.update_file.assert_called_once()
-        destination_repo.update_file.assert_called_with(
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.return_value.sha = '456'
+        self.github_util._update_file(source_file, branch='main')
+        self.github_util.repository.update_file.assert_called_once()
+        self.github_util.repository.update_file.assert_called_with(
             path='foo', message='Updated by file-sync', content=b'foo', sha='456', branch='main'
         )
 
     def test_update_file_should_create_a_new_file(self: Self) -> None:
         """Validate the update_file function is successful."""
         source_file = FileConfig(source_path='', destination_path='foo', content=b'foo')
-        destination_repo = MagicMock()
-        destination_repo.get_contents.side_effect = UnknownObjectException(404)
-        self.github_util.update_file(destination_repo, source_file, branch='main')
-        destination_repo.create_file.assert_called_once()
-        destination_repo.create_file.assert_called_with(
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.side_effect = UnknownObjectException(404)
+        self.github_util._update_file(source_file, branch='main')
+        self.github_util.repository.create_file.assert_called_once()
+        self.github_util.repository.create_file.assert_called_with(
             path='foo', message='Updated by file-sync', content=b'foo', branch='main'
         )
 
     def test_create_pull_request(self: Self) -> None:
         """Validate the create_pull_request function is successful."""
-        repo = MagicMock()
-        self.github_util.create_pull_request(repo, 'test-branch')
-        repo.create_pull.assert_called_once()
-        repo.create_pull.assert_called_with(
+        self.github_util.repository = MagicMock()
+        self.github_util._create_pull_request('test-branch')
+        self.github_util.repository.create_pull.assert_called_once()
+        self.github_util.repository.create_pull.assert_called_with(
             title='file-sync', head='test-branch', base='main'
         )
 
-        repo = MagicMock()
-        repo.create_pull.side_effect = GithubException(status=422, data={'message': 'Invalid request'})
-        self.github_util.create_pull_request(repo, 'test-branch')
-        repo.create_pull.assert_called_once()
-        repo.create_pull.assert_called_with(
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.create_pull.side_effect = GithubException(status=422,
+                                                                              data={'message': 'Invalid request'})
+        self.github_util._create_pull_request('test-branch')
+        self.github_util.repository.create_pull.assert_called_once()
+        self.github_util.repository.create_pull.assert_called_with(
             title='file-sync', head='test-branch', base='main'
         )
 
@@ -119,18 +124,15 @@ class TestGitHubUtil(unittest.TestCase):
         template_file.sha = '123'
         template_file.content = 'Zm9vCg=='
 
-        source_repo = MagicMock()
-        source_repo.get_contents.return_value = template_file
-        self.github_session.get_repo.return_value = source_repo
+        self.github_util.repository = MagicMock()
+        self.github_util.repository.get_contents.return_value = template_file
 
-        sync_files = self.github_util.get_sync_files_from_source_repo(
-            source_repo_name='my-org/test-repo-1', action_input_files='foo/bar.yml', source_branch='main'
-        )
+        sync_files = self.github_util.get_sync_files_from_source_repo(action_input_files='foo/bar.yml', branch='main')
         expected = [FileConfig(source_path='foo/bar.yml', destination_path='foo/bar.yml', content=b'foo\n', sha='123')]
         self.assertEqual(expected, sync_files)
 
-        self.github_session.get_repo.assert_called_once()
-        self.github_session.get_repo.assert_called_with('my-org/test-repo-1')
+        self.github_util.repository.get_contents.assert_called_once()
+        self.github_util.repository.get_contents.assert_called_with('foo/bar.yml', ref='main')
 
-        source_repo.get_contents.assert_called_once()
-        source_repo.get_contents.assert_called_with('foo/bar.yml', ref='main')
+    def test_sync_files_for_repo(self: Self) -> None:
+        pass
